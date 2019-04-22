388d548d0fc639ad14c5cb47dfdd5481c0135457
Odd-Tørres Lunde <oddtlu@nomacoddtlu.domain_not_set.invalid>
2019-04-22 13:22:15 +0200

    MVP scoreboard downloader

---
+++ download.js
@@ -0,0 +1,12 @@
+const http = require('https');
+const fs = require('fs');
+
+function download(matchId) {
+  console.log(matchId)
+  const request = http.get("https://cryptic-plateau-12043.herokuapp.com/scoreboard/png?matchId=" + matchId, function(response) {
+    const file = fs.createWriteStream(matchId + ".png")  
+    response.pipe(file);
+  });
+}
+
+module.exports = download
---
+++ server.js
@@ -0,0 +1,21 @@
+const download = require("./download")
+const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', async(req, res) => {
+    repeatTwoHours(req.query.matchId)
+    await download(req.query.matchId)
+    res.send("done")
+})
+
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
+
+function repeatTwoHours(matchId) {
+    const intervalId = setInterval(() => download(matchId), 1000)
+    setTimeout(() => {
+        clearInterval(intervalId)
+       fs.unlink("./" + matchId + ".png", () => console.log("deleted " + matchId))
+    }, 10000)
+}
