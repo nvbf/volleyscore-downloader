20e5429687b98b568fbfcdcf3dff940bd6b08304
Odd-Tørres Lunde <oddtlunde@gmail.com>
2019-04-25 19:48:53 +0200

    endret path

--- download.js
+++ download.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 function download(matchId) {
   console.log(matchId)
   const request = http.get("https://cryptic-plateau-12043.herokuapp.com/scoreboard/png?matchId=" + matchId, function(response) {
-    const file = fs.createWriteStream(matchId + ".png")  
+    const file = fs.createWriteStream(__dirname + "/" + matchId + ".png")  
     response.pipe(file);
   });
 }
