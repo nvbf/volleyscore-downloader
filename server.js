ba8b61ff7c3b2a7d88c764675dbd951c9fc20e5a
Odd-Tørres Lunde <oddtlunde@gmail.com>
2019-06-19 19:31:07 +0000

    Adding reconfig

--- server.js
+++ server.js
@@ -1,26 +1,80 @@
 const download = require("./download")
-const restart = restart("./restart")
+const restartsrs = require("./restart")
 const fs = require('fs');
 const express = require('express')
+const srsconfig = require('./srsconfig')
 const app = express()
 const port = 3000
 
 const data = [] 
+
+const courts_conf = {
+    1: {
+       "mid": "match_id_court1",
+       "sid": "stream_id_court1",
+    },
+    2: {
+       "mid": "match_id_court2",
+       "sid": "stream_id_court2",
+    },
+    3: {
+       "mid": "match_id_court3",
+       "sid": "stream_id_court3",
+    },
+    4: {
+       "mid": "match_id_court4",
+       "sid": "stream_id_court4",
+    }
+}
+
 app.get('/', async(req, res) => {
+
     if (!req.query.matchId) {
         res.send("Oppgi matchId");
         return;
     }
+    if (!req.query.eventId) {
+        res.send("Oppgi eventId");
+        return;
+    }
+    if (!req.query.courtNum) {
+        res.send("Oppgi courtNum");
+        return;
+    }
+    if (!req.query.tournamentNum) {
+        res.send("Oppgi tournamentNum");
+        return;
+    }
 
-    if(data.some((obj) => obj === req.query.matchId)) {
+    const tournamentid = req.query.tournamentId;
+    const eventid = req.query.eventId;
+    const courtnum = req.query.courtNum;
+    const matchid = req.query.matchId;
+
+	if (courtnum > 4 || courtnum < 0) {
+        res.send("Invalid court number. Should be 1-4");
+        return;
+	}
+
+	courts_conf[courtnum]["mid"] = matchid;
+	courts_conf[courtnum]["sid"] = eventid;
+
+	srsconfig(
+		courts_conf[1]["mid"], courts_conf[1]["sid"],
+		courts_conf[2]["mid"], courts_conf[2]["sid"],
+		courts_conf[3]["mid"], courts_conf[3]["sid"],
+		courts_conf[4]["mid"], courts_conf[4]["sid"]
+	)
+	restartsrs()
+
+    if(data.some((obj) => obj === matchid)) {
         res.send("Prosesseres allerede")
         return;
     }
 
-    data.push(req.query.matchId)
-    restart()
-    repeatTwoHours(req.query.matchId)
-    await download(req.query.matchId)
+    data.push(matchid)
+    repeatTwoHours(matchid)
+    await download(matchid)
 
     res.send("Done")
 })
---
+++ srsconfig.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+
+function genConfig(
+    match_id_court1, stream_id_court1,
+    match_id_court2, stream_id_court2,
+    match_id_court3, stream_id_court3,
+    match_id_court4, stream_id_court4) 
+{
+	const conf_str = `
+# the config for srs to delivery stream for OSVB, rtmp + ffmpeg
+
+listen              1935;
+srs_log_tank        file;
+srs_log_file        /root/SRS-CentOS6-x86_64-2.0.258/usr/local/srs/objs/osvb2-srs.log;
+pid            /root/SRS-CentOS6-x86_64-2.0.258/usr/local/srs/objs/osvb2.pid;
+max_connections     1000;
+vhost __defaultVhost__ {
+transcode {
+	enabled     on;
+	ffmpeg      /root/bin/ffmpeg;
+	engine court1 {
+
+	    enabled         on;
+	    vfilter {
+	i /root/volleyscore-downloader/${match_id_court1}.png;
+	filter_complex  'overlay=10:10';
+	f image2;
+	    }
+	    vcodec          libx264;
+	    vthreads        4;
+	    vprofile        main;
+	    vpreset         medium;
+	    vparams {
+	    }
+	    acodec          libfdk_aac;
+	    aparams {
+	    }
+	    output         rtmp://a.rtmp.youtube.com/live2/${stream_id_court1};
+	}
+
+	engine court2 {
+
+	    enabled         on;
+	    vfilter {
+	i /root/volleyscore-downloader/${match_id_court2}.png;
+	filter_complex  'overlay=10:10';
+	f image2;
+	    }
+	    vcodec          libx264;
+	    vthreads        4;
+	    vprofile        main;
+	    vpreset         medium;
+	    vparams {
+	    }
+	    acodec          libfdk_aac;
+	    aparams {
+	    }
+	    output         rtmp://a.rtmp.youtube.com/live2/${stream_id_court2};
+	}
+
+	engine court3 {
+
+	    enabled         on;
+	    vfilter {
+	i /root/volleyscore-downloader/${match_id_court3}.png;
+	filter_complex  'overlay=10:10';
+	f image2;
+	    }
+	    vcodec          libx264;
+	    vthreads        4;
+	    vprofile        main;
+	    vpreset         medium;
+	    vparams {
+	    }
+	    acodec          libfdk_aac;
+	    aparams {
+	    }
+	    output         rtmp://a.rtmp.youtube.com/live2/${stream_id_court3};
+	}
+
+	engine court4 {
+
+	    enabled         on;
+	    vfilter {
+	i /root/volleyscore-downloader/${match_id_court4}.png;
+	filter_complex  'overlay=10:10';
+	f image2;
+	    }
+	    vcodec          libx264;
+	    vthreads        4;
+	    vprofile        main;
+	    vpreset         medium;
+	    vparams {
+	    }
+	    acodec          libfdk_aac;
+	    aparams {
+	    }
+	    output         rtmp://a.rtmp.youtube.com/live2/${stream_id_court4};
+	}
+    }
+}
+	`
+
+	fs.writeFileSync(
+                "/root/SRS-CentOS6-x86_64-2.0.258/usr/local/srs/conf/osvb2.conf", 
+		conf_str, function(err) 
+	{
+
+	    if (err) {
+		return console.log(err);
+	    }
+	    console.log("OSVB config file updated");
+	}); 
+}
+
+module.exports = genConfig
